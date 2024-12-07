5253ff0d2a86c8c38a85e78745e3bd889f77737c
Jiří Ogurek <j_ogurek@seznam.cz>
2024-12-07 16:06:33 +0100

    Day03 commit

--- Day03/Day03.cs
+++ Day03/Day03.cs
@@ -1,26 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace AOC2024.Day02
+namespace AOC2024.Day03
 {
     internal static class Day03
     {
         public static void Solve()
         {
             var input = File.ReadAllText(@"Day03\input.txt");
-
-            //var stopwatch = Stopwatch.StartNew();
-            //PartOne(reportList);
-            //stopwatch.Stop();
-            //Console.WriteLine($"PartOne execution time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+            PartOne(input);
+            PartTwo(input);
         }
 
-        private static void PartOne(List<List<int>> reportList)
+        private static void PartOne(string input)
         {
+            var matches = FindMulInstances(input, @"mul\((\d+),(\d+)\)");
+            var sum = matches.Sum(match => match.X * match.Y);
+            Console.WriteLine(sum);
         }
+
+        private static void PartTwo(string input)
+        {
+            var matches = FindMulInstances(input, @"mul\((\d+),(\d+)\)");
+            var donts = FindIndexes(input, @"don't\(\)");
+            var dos = FindIndexes(input, @"do\(\)");
+
+            var validIndexes = GetValidIndexes(dos, donts, input.Length);
+
+            var sum = matches.Where(match => validIndexes[match.Index].Item2).Sum(match => match.X * match.Y);
+
+            Console.WriteLine(sum);
+        }
+
+        private static List<(int, bool)> GetValidIndexes(List<int> dos, List<int> donts, int length)
+        {
+            var result = new List<(int, bool)>();
+            var state = true;
+            for (var i = 0; i < length; i++)
+            {
+                if (donts.Contains(i))
+                    state = false;
+                else
+                    if (dos.Contains(i))
+                        state = true;
+
+                result.Add((i, state));
+            }
+            return result;
+        }
+
+        private static List<MulInstance> FindMulInstances(string input, string pattern)
+        {
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+
+            var result = new List<MulInstance>();
+            foreach (Match match in matches)
+            {
+                var x = int.Parse(match.Groups[1].Value);
+                var y = int.Parse(match.Groups[2].Value);
+                var index = match.Index;
+
+                result.Add(new MulInstance
+                {
+                    X = x,
+                    Y = y,
+                    Index = index
+                });
+            }
+
+            return result;
+        }
+
+        private static List<int> FindIndexes(string input, string pattern)
+        {
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(input);
+
+            var result = new List<int>();
+            foreach (Match match in matches)
+            {
+                result.Add(match.Index);
+            }
+
+            return result;
+        }
+    }
+
+    internal class MulInstance
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Index { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using AOC2024.Day01;
 using AOC2024.Day02;
+using AOC2024.Day03;
 
 //Day01.Solve();
 //Day02.Solve();
