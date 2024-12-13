2e12f15c4f8692f6608eb5397fff37b8f047bb0f
Jiří Ogurek <j_ogurek@seznam.cz>
2024-12-13 13:35:50 +0100

    Day06 done

--- Day06/Day06.cs
+++ Day06/Day06.cs
@@ -7,17 +7,156 @@ namespace AOC2024.Day06
     internal class Day06
     {
         private char[,] Array { get; set; }
-        private (int, int) Coords => GetCoords(Array);
-        private char Direction => GetGuardDirection(Array, Coords);
+        private (int, int, char) _state;
+        private (int, int, char) State
+        {
+            get => _state;
+            set => SetCoords(value);
+        }
+        private List<(int, int, char)> Visited { get; set; }
+
+        private void SetCoords((int, int, char) value)
+        {
+            if (value.Item1 != _state.Item1 || value.Item2 != _state.Item2)
+                Visited.Add(value);
+
+            _state = value;
+        }
+
+        private (int, int, char) InitState;
 
         public void Solve()
         {
             PartOne();
+            PartTwo();
+        }
+
+        private void PartTwo()
+        {
+            Array = CreateArray(@"Day06\input.txt");
+            Visited = [];
+            State = GetInitialState(Array);
+            InitState = State;
+
+            var loops = 0;
+
+            var rows = Array.GetLength(0);
+            var cols = Array.GetLength(1);
+            for (var x = 0; x < rows; x++)
+            {
+                for (var y = 0; y < cols; y++)
+                {
+                    if (Array[x, y] == '.')
+                    {
+                        Array[x, y] = '#';
+
+                        State = InitState;
+                        Visited = [];
+
+                        if (GetStepsCount() == -1)
+                            loops++;
+
+                        Array[x, y] = '.';
+                    }
+
+                    if ((x + 1) * (y + 1) % (rows * cols / 100) == 0)
+                        Console.WriteLine($"{x * y / (rows * cols / 100)}% done");
+                }
+            }
+
+            Console.WriteLine(loops);
         }
 
         private void PartOne()
         {
             Array = CreateArray(@"Day06\input.txt");
+            Visited = [];
+            State = GetInitialState(Array);
+
+            Console.WriteLine(GetStepsCount());
+        }
+
+        private int GetStepsCount()
+        {
+            while (IsInBounds())
+            {
+                Array[State.Item1, State.Item2] = '.';
+                Move();
+                if (Visited.Where(x => x == State).GroupBy(n => n).Any(g => g.Count() > 1))
+                    return -1;
+            }
+
+            if (Visited.Any())
+                Visited.RemoveAt(Visited.Count - 1);
+            
+            return Visited.Select(x => (x.Item1, x.Item2)).Distinct().Count();
+        }
+
+        private void Move()
+        {
+            while (true)
+            {
+                try
+                {
+
+                    switch (State.Item3)
+                    {
+                        case '^':
+                            if (Array[State.Item1 - 1, State.Item2] != '#')
+                                State = (State.Item1 - 1, State.Item2, State.Item3);
+                            else
+                            {
+                                State = (State.Item1, State.Item2, '>');
+                                continue;
+                            }
+
+                            break;
+                        case '>':
+                            if (Array[State.Item1, State.Item2 + 1] != '#')
+                                State = (State.Item1, State.Item2 + 1, State.Item3);
+                            else
+                            {
+                                State = (State.Item1, State.Item2, 'v');
+                                continue;
+                            }
+
+                            break;
+                        case 'v':
+                            if (Array[State.Item1 + 1, State.Item2] != '#')
+                                State = (State.Item1 + 1, State.Item2, State.Item3);
+                            else
+                            {
+                                State = (State.Item1, State.Item2, '<');
+                                continue;
+                            }
+
+                            break;
+                        case '<':
+                            if (Array[State.Item1, State.Item2 - 1] != '#')
+                                State = (State.Item1, State.Item2 - 1, State.Item3);
+                            else
+                            {
+                                State = (State.Item1, State.Item2, '^');
+                                continue;
+                            }
+
+                            break;
+                    }
+
+                    break;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    State = (-1, -1, '-');
+                    return;
+                }
+            }
+        }
+
+        private bool IsInBounds()
+        {
+            var (x, y, _) = State;
+            return x >= 0 && x < Array.GetLength(0) && y >= 0 && y < Array.GetLength(1);
         }
 
         private static char[,] CreateArray(string input)
@@ -44,7 +183,7 @@ namespace AOC2024.Day06
             return array;
         }
 
-        private static (int, int) GetCoords(char[,] array)
+        private static (int, int, char) GetInitialState(char[,] array)
         {
             var rows = array.GetLength(0);
             var cols = array.GetLength(1);
@@ -54,17 +193,11 @@ namespace AOC2024.Day06
                 {
                     if (array[x, y] != '.' && array[x, y] != '#')
                     {
-                        return (x, y);
+                        return (x, y, array[x, y]);
                     }
                 }
             }
-            return (-1, -1);
-        }
-
-        private static char GetGuardDirection(char[,] array, (int, int) coords)
-        {
-            var (x, y) = coords;
-            return array[x, y];
+            return (-1, -1, '-');
         }
     }
 }
